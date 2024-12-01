16ed512346e12fe3383427df09c5e3abb530c5c0
Jaber-Ahmed77 <jaber333513@gmail.com>
2024-12-01 13:43:25 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 import HeroSection from "./components/HeroSection/HeroSection";
 import MainTitle from "./components/ui/Title/MainTitle";
 import OurProducts from "./components/OurProductsSection.jsx/OurProducts";
@@ -15,7 +15,7 @@ export default function Home() {
 
   useEffect(() => {
     Aos.init({
-      duration: 300, // Global animation duration
+      duration: 2000, // Global animation duration
       easing: "ease-in-out", // Global easing
     });
   }, []);
@@ -23,10 +23,10 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col gap-10 lg:gap-20">
       <div className="w-full">
-        <div className=" absolute top-0 left-0 z-[-1] w-full h-[940px] bg-main_blue-900 bg-hero-bg bg-center md:bg-[100px, 5px] bg-Cover"></div>
+        <div className=" absolute top-0 left-0 z-[-1] w-full h-[749px] bg-main_blue-900 bg-hero-bg bg-center md:bg-[100px, 5px] bg-Cover"></div>
 
         {/* image desktop */}
-        <div className="hidden md:block absolute w-[760px] md:w-[1050px] xl:w-[1400px] 2xl:w-[1730px] h-[440px] md:h-[600px]  md:-bottom-20 xl:bottom-10 -left-[50%] md:-left-[13%] 2xl:-left-20 z-[-1]">
+        {/* <div className="hidden md:block absolute w-[760px] md:w-[1050px] xl:w-[1400px] 2xl:w-[1730px] h-[440px] md:h-[600px]  md:-bottom-20 xl:bottom-10 -left-[50%] md:-left-[13%] 2xl:-left-20 z-[-1]">
           <Image
             src="/img/lines-desktop.svg"
             alt="quote"
@@ -34,18 +34,43 @@ export default function Home() {
             quality={70}
             className="object-cover"
           />
-        </div>
+        </div> */}
 
-        {/* image mob */}
-        <div className="block md:hidden overflow-x-clip absolute bottom-[40%] z-[-1]">
-          <Image
+        {/* <div className="hidden md:block absolute top-0 left-0 w-screen h-[851px] z-[-1]">
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-2]"
+            src="/video/bg-video.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+
+          <div className="absolute inset-0 bg-[#12475F91] bg-opacity-[57%]"></div>
+        </div> */}
+
+          {/* image mob */}
+          <div className="block md:hidden absolute top-0 left-0 w-screen h-[940px] z-[-1]">
+          {/* <Image
             src="/img/lines-desktop.svg"
             alt="quote"
             width={900}
             height={600}
             quality={70}
             className="scale-[1.7]"
+          /> */}
+
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-2]"
+            src="/video/bg-video.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
           />
+
+          {/* Overlay (optional for darkening) */}
+          <div className="absolute inset-0 bg-[#12475F91] bg-opacity-[57%]"></div>
         </div>
 
         <HeroSection />
